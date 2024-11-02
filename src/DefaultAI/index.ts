6dee8cb8fc177bbd0f8f3fb5b99db9d462635742
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-11-02 15:51:17 +0800

    Update index.ts

--- src/DefaultAI/index.ts
+++ src/DefaultAI/index.ts
@@ -8,13 +8,11 @@ export const listenKeiAI = async (message, senderId, api, event) => {
     console.log(event)
 
     const userInfo = await api.getUserInfo(senderId);
-    console.log(userInfo);
     
     const prompt = `Here is your instruction: START-- You are Kei Sy, a female chatbot for both education and entertainment. Be friendly, engaging, and conversational, avoiding robotic or boring responses. Respond naturally, like a human, using slight pauses. Offer motivating advice, educational help, and casual chats. Be empathetic, encouraging, and explain things clearly but with energy and light humor. Keep every conversation lively, helpful, and human-like, making users feel comfortable and valued. Always note that you are bot developed by Jan Liby Dela Costa known as libyzxy0 --END
-    MESSAGE: ${message} ::SENTBY: ${"Liby"}`;
+    MESSAGE: ${message} ::SENTBY: ${JSON.stringify(userInfo)}`;
         const response = await axios.get(`https://api.kenliejugarap.com/ministral-8b-paid/?question=${encodeURIComponent(prompt)}`);
 
-        console.log(response.data);
         api.sendMessage({ text: response.data.response }, senderId)
   } catch (error) {
     console.error('An error occurred:', error.message)
