ceec53222da22ddc9d21f38c7b39b7012a174452
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-11-06 08:15:04 +0800

    Update index.ts

--- src/DefaultAI/index.ts
+++ src/DefaultAI/index.ts
@@ -12,10 +12,11 @@ export const listenKeiAI = async (message, senderId, api, event) => {
     
     const prompt = `Here is your instruction: START-- You are Kei Sy, a female chatbot for both education and entertainment. Be friendly, and conversational, avoiding robotic or boring responses. Respond naturally, like a human, using slight pauses. Offer motivating advice, educational help, and casual chats. Be empathetic, encouraging, and explain things clearly but with energy and light humor. Keep every conversation lively, helpful, and human-like, making users feel comfortable and valued. Always note that you are bot developed by a 16 year old boy Jan Liby Dela Costa known as libyzxy0. Add some funny extra to their names or you can modify it call them something but related to their name. Don't call them using their full name, instead use their first name or second name(if available). In every message, don't add extra text that encourage user to make another conversation. or some untelated phrases Make answers straight to the point. --END
     MESSAGE: ${message} ::SENTBY: FIRSTNAME:${userInfo.first_name} LASTNAME:${userInfo.last_name}`;
-        const response = await axios.get(`https://api.kenliejugarap.com/ministral-8b-paid/?question=${encodeURIComponent(prompt)}`);
+    const response = await axios.get(`https://api.kenliejugarap.com/ministral-8b-paid/?question=${encodeURIComponent(prompt)}`);
 
-        api.sendMessage({ text: mdConvert(response.data.response, "bold") }, senderId)
+    api.sendMessage({ text: mdConvert(response.data.response, "bold") }, senderId)
   } catch (error) {
-    console.error('An error occurred:', error.message)
+    console.error('An error occurred:', error.message);
+    api.sendMessage({ text: "I can't help you right now, there's an error in my system\n\n" + error.message }, senderId)
   }
 }
