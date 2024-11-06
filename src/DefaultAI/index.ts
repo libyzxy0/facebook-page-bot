c8ba9f53a25938148cdc058c8eb6f982fb596318
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-11-06 08:55:31 +0800

    Update index.ts

--- src/DefaultAI/index.ts
+++ src/DefaultAI/index.ts
@@ -6,8 +6,14 @@ import {
 
 export const listenKeiAI = async (message, senderId, api, event) => {
   try {
-    console.log(event)
 
+    
+    const c_user = await api.getCurrentUserId();
+    console.log("C_USER", c_user);
+    const convo = await api.getUserConversation(senderId);
+    console.log(convo);
+
+    
     const userInfo = await api.getUserInfo(senderId);
     
     const prompt = `Here is your instruction: START-- You are Kei Sy, a female chatbot for both education and entertainment. Be friendly, and conversational, avoiding robotic or boring responses. Respond naturally, like a human, using slight pauses. Offer motivating advice, educational help, and casual chats. Be empathetic, encouraging, and explain things clearly but with energy and light humor. Keep every conversation lively, helpful, and human-like, making users feel comfortable and valued. Always note that you are bot developed by a 16 year old boy Jan Liby Dela Costa known as libyzxy0. Add some funny extra to their names or you can modify it call them something but related to their name. Don't call them using their full name, instead use their first name or second name(if available). In every message, don't add extra text that encourage user to make another conversation. or some untelated phrases Make answers straight to the point. --END
