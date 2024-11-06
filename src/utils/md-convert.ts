d7a59b85d654c7e4adb91285d07156439197b001
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-11-06 11:04:00 +0800

    Update md-convert.ts

--- src/utils/md-convert.ts
+++ src/utils/md-convert.ts
@@ -1,11 +1,26 @@
-import {
-  fontText
-} from '@/utils/fonts'
+import { fontText } from '@/utils/fonts';
 
-export function mdConvert(md, fontType) {
+export function mdConvert(md: string) {
   const boldRegex = /\*\*(.*?)\*\*/g;
+  const headingRegex = /^(#{1,6})\s*(.*?)$/gm;
+  const codeRegex = /`(.*?)`/g;
+  const codeBlockRegex = /```[a-zA-Z]*\s*(.*?)\s*```/gs;
 
-  return md.replace(boldRegex, (match, p1) => {
-    return fontText(p1, fontType);
+  md = md.replace(boldRegex, (match, p1) => {
+    return fontText(p1, 'bold');
   });
+
+  md = md.replace(headingRegex, (match, hashes, p1) => {
+    return fontText(p1, 'bold');
+  });
+
+  md = md.replace(codeRegex, (match, p1) => {
+    return fontText(p1, 'sansSerifItalic');
+  });
+
+  md = md.replace(codeBlockRegex, (match, p1) => {
+    return p1;
+  });
+
+  return md;
 }
